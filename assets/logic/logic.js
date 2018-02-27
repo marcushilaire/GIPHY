1c89806ec67abdaf2e40ffc1ac8ce2f73522b44b
unknown <marcushilaire@gmail.com>
2018-02-26 19:01:02 -0500

    first iteration of ajax call

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -19,3 +19,26 @@ $("#addQuery").on("click", function(event){
     searchTerms.push(newQuery);
     buttonGenerate();
 })
+$(".terms").on("click", function(){
+    var term = $(this).attr("data-term");
+    console.log(term);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+term+"&api_key=faVpcYpz55WtmrGnbEZLrtW06l9qyXdi&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        // console.log(response.data);
+        var imageSet =response.data;
+        console.log(imageSet);
+        for(var i=0; i<imageSet.length; i++){
+        var div = $("<div>");
+        var rating = $("<p>").text(imageSet[i].rating);
+        console.log(rating);
+        var img = $("<img>").attr({
+            "src": imageSet[i].images.fixed_height_still.url,
+        })
+        div.append(rating, img);
+        $("#imgDump").append(div)
+        }})
+    
+})
