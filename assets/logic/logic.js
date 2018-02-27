a240c4a091a2d0c9812e13d3e8440b037a3a8355
unknown <marcushilaire@gmail.com>
2018-02-26 20:58:54 -0500

    complete functionality

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,4 +1,3 @@
-console.log("link test")
 var searchTerms = ["Greys Anatomy", "My Hero Academia", "Arrested Development", "Cats", "Dogs", "Luther", "Brooklyn 99", ]
 var buttonGenerate = function(){
     $("#buttonsDiv").empty();
@@ -10,7 +9,7 @@ var buttonGenerate = function(){
         })
         $("#buttonsDiv").append(button);
     }
-}
+};
 buttonGenerate();
 $("#addQuery").on("click", function(event){
     event.preventDefault();
@@ -18,8 +17,8 @@ $("#addQuery").on("click", function(event){
     console.log(newQuery);
     searchTerms.push(newQuery);
     buttonGenerate();
-})
-$(".terms").on("click", function(){
+});
+$(document).on("click",".terms", function(){
     var term = $(this).attr("data-term");
     console.log(term);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+term+"&api_key=faVpcYpz55WtmrGnbEZLrtW06l9qyXdi&limit=10";
@@ -33,12 +32,32 @@ $(".terms").on("click", function(){
         for(var i=0; i<imageSet.length; i++){
         var div = $("<div>");
         var rating = $("<p>").text(imageSet[i].rating);
-        console.log(rating);
         var img = $("<img>").attr({
             "src": imageSet[i].images.fixed_height_still.url,
+            "class": "gifs",
+            "data-still": imageSet[i].images.fixed_height_still.url,
+            "data-active":imageSet[i].images.fixed_height.url,
+            "data-state": "still"
         })
         div.append(rating, img);
         $("#imgDump").append(div)
-        }})
-    
+        }})  
+});
+$(document).on("click", ".gifs", function(){
+    console.log("please work")
+// })
+// $(".gifs").on("click", function(){
+    var state = $(this).attr("data-state");
+    console.log(state);
+    var pause = $(this).attr("data-still");
+    console.log(pause);
+    var play = $(this).attr("data-active");
+    console.log(play);
+    if (state == "still"){
+        $(this).attr("src", play);
+        $(this).attr("data-state", "active");
+    } else{
+        $(this).attr("src", pause);
+        $(this).attr("data-state", "still");
+    }
 })
